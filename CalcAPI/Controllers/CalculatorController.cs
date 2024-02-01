10e485c4a50d30ccc3bd888153015c4bd398524a
Osamp <157659771+Osamp@users.noreply.github.com>
2024-02-01 00:59:33 +0000

    commited first assignment

---
+++ CalcAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]/[action]")]
+
+public class CalculatorController : ControllerBase
+{
+        [HttpGet]
+    public IActionResult Divide(decimal numerator, decimal denominator)
+    {
+        if(denominator == 0)
+        {
+            // Console.WriteLine("Error: Division by zero");
+            return BadRequest("Error: Division by zero");
+        }
+        else
+        {   decimal result= numerator / denominator;
+            return Ok(result);
+        }
+    }
+
+    [HttpGet]
+
+    public IActionResult Multiply(decimal numerator, decimal denominator)
+    {       
+        decimal result= numerator*denominator;
+        return Ok(result);
+    }
+
+
+    [HttpGet]
+
+    public IActionResult Add(decimal numerator, decimal denominator)
+    {       
+        decimal result= numerator*denominator;
+        return Ok(result);
+    }
+    
+
+    [HttpGet]
+
+
+ public IActionResult Subtract(decimal numerator, decimal denominator)
+    {       
+        decimal result= numerator*denominator;
+        return Ok(result);
+    }
+   
+
+    [HttpGet]
+
+    public IActionResult Modulus(decimal numerator, decimal denominator)
+    {
+        if(denominator == 0)
+        {
+            return BadRequest("Error: Division by zero");
+        }
+        else
+        {
+            decimal result=numerator%denominator;
+            return Ok(result);
+            
+        }
+    }
+}
+
+   
