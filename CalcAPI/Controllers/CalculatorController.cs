3884c689075fb49c10eed9fd0f43f2f27a7c93a6
Osamp <157659771+Osamp@users.noreply.github.com>
2024-02-14 05:57:43 +0000

    my first commit

--- CalcAPI/Controllers/CalculatorController.cs
+++ CalcAPI/Controllers/CalculatorController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+namespace CalculatorAPI.controllers;
 
 [ApiController]
 [Route("[controller]/[action]")]
---
+++ CalcAPITests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CalculatorAPI.controllers;
+namespace CalcAPITests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
